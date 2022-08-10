5a3734a3e9da1d35b21469818dd986313393ccb2
Alina <pisarenko.alina@gmail.com>
2022-08-10 13:43:16 -0400

    added json server, fetching functionality

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'nika', age: 33 });
+const user = new User({ name: 'alina', age: 30 });
 
-user.set({ name: 'elena' });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.save();
